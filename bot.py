0a4d19fd8e5076d24ae98d62df4904483fa8a6da
vano31 <vanosike31@gmail.com>
2024-10-25 17:01:54 -0400

    Server name retrieval set up

--- bot.py
+++ bot.py
@@ -3,14 +3,28 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='.',intents=intents)
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id:{guild.id})'
+    )
+    #print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
+
